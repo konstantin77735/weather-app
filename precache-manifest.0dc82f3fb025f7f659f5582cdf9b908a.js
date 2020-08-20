e89f1ae6a676eab12d70aedf108a305a449c1304
= <=>
2020-08-20 22:00:17 +0300

    change to Vladikavkaz

---
+++ precache-manifest.0dc82f3fb025f7f659f5582cdf9b908a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be60ef2485cb0ce9018a995c98a00bf5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "fc2a7cbc23b8061b0c06",
+    "url": "/weather-app/static/css/main.e0973c2e.chunk.css"
+  },
+  {
+    "revision": "3b65c16ea6c021ea4858",
+    "url": "/weather-app/static/js/2.62978037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.62978037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2a7cbc23b8061b0c06",
+    "url": "/weather-app/static/js/main.e5b0a197.chunk.js"
+  },
+  {
+    "revision": "7ac77cfad60c76683120",
+    "url": "/weather-app/static/js/runtime-main.fcb9738c.js"
+  }
+]);
